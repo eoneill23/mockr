004ef35a86fc52964bdf18049fb7137fefc3c05e
Eric O'Neill <oneill.eric23@gmail.com>
2019-10-21 14:50:50 -0600

    Update component file structure and create HomePage component directory

---
+++ src/components/App/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import HomePage from '../HomePage/HomePage';
+
+export const App = () => {
+  return (
+    <HomePage />
+  );
+}
+
+export default App;
---
+++ src/components/HomePage/HomePage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const HomePage = () => {
+  return (
+    <div>Hello</div>
+  )
+}
+
+export default HomePage;
