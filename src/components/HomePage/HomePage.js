7de9f97d481a2d5e646a4bae37b9ecb09d0d9d7a
Aurie <Myrdden42@gmail.com>
2019-10-22 16:40:31 -0600

    Removed Obsolete Component

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,6 +1,31 @@
 import React from 'react';
 import './HomePage.css';
 
+import gql from 'graphql-tag';
+import {useQuery} from '@apollo/react-hooks';
+
+const QueryResult = () => {
+  const QUESTIONS = gql`
+    query {
+      questions {
+        id
+        body
+        active
+      }
+    }
+  `;
+
+  const {loading, error, data} = useQuery(QUESTIONS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error...</p>;
+
+  console.log(data);
+  return data.questions.map(({id, body}) => {
+    return <p key={id}>{body}</p>
+  });
+}
+
 export const HomePage = () => {
   return (
     <section className='welcome-info'>
@@ -13,6 +38,8 @@ export const HomePage = () => {
       <p>
         Click log in at the top right corner of the page to log in an begin the mock interview process!
       </p>
+
+      <QueryResult/>
     </section>
   )
 }
