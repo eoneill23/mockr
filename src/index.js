352291379085ca0741d682b85fbabf78198ec108
Aurie <Myrdden42@gmail.com>
2019-10-22 16:23:02 -0600

    Setup Apollo Provider

--- src/index.js
+++ src/index.js
@@ -7,8 +7,9 @@ import * as serviceWorker from './serviceWorker';
 
 // Apollo
 import ApolloClient from 'apollo-boost';
+import {ApolloProvider} from '@apollo/react-hooks';
 const client = new ApolloClient({
-  uri: 'https://thawing-wave-76846.herokuapp.com'
+  uri: 'https://thawing-wave-76846.herokuapp.com/graphql'
 });
 
 ReactDOM.render(
