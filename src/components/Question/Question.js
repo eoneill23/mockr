f9f6b2fdc622dbc9bf70a9fa1adb8c0cf5d0d43f
Aurie <Myrdden42@gmail.com>
2019-10-22 13:37:34 -0600

    Recreate Question Component as Function

---
+++ src/components/Question/Question.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export const Question = props => {
+  const whereAmI(index) {
+    let pos = parseInt(props.pos);
+    if (pos < index) {
+      return 'card-finished';
+    } else if (pos === index) {
+      return 'card-focused';
+    } else {
+      if (pos === (index + 1)) {
+        return 'card-first';
+      } else if (pos === (index + 2)) {
+        return 'card-second';
+      } else if (pos === (index + 3)) {
+        return 'card-third';
+      } else {
+        return 'card-hidden';
+      }
+    }
+  }
+
+  return (
+    <div className={'card shadow ' + whereAmI(props.index)}>
+      <h1>Q: Something</h1>
+    </div>
+  );
+}
