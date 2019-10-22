234cf03c6145b6a092580262265b1f0d79912043
Aurie <Myrdden42@gmail.com>
2019-10-22 15:41:00 -0600

    Added Enzyme Adapter to Test Files

--- src/components/NavBar/NavBar.test.js
+++ src/components/NavBar/NavBar.test.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import NavBar from './NavBar';
 
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
 describe('NavBar', () => {
   let wrapper;
 
