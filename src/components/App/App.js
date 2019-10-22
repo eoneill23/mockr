a6287f36dc50b033aaa5bbf64f3f22e5a3f56e72
Aurie <Myrdden42@gmail.com>
2019-10-22 17:01:19 -0600

    Fixed Test

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,14 +6,23 @@ import QuestionDeck from '../QuestionDeck/QuestionDeck';
 import { Route } from 'react-router-dom';
 import './App.css';
 
+// Apollo
+import ApolloClient from 'apollo-boost';
+import {ApolloProvider} from '@apollo/react-hooks';
+const client = new ApolloClient({
+  uri: 'https://thawing-wave-76846.herokuapp.com/graphql'
+});
+
 export const App = () => {
   return (
-    <main>
-      <NavBar />
-      <Route exact path='/'><HomePage /></Route>
-      <Route exact path='/login'><LoginForm /></Route>
-      <Route exact path='/questions'><QuestionDeck /></Route>
-    </main>
+    <ApolloProvider client={client}>
+      <main>
+        <NavBar />
+        <Route exact path='/'><HomePage /></Route>
+        <Route exact path='/login'><LoginForm /></Route>
+        <Route exact path='/questions'><QuestionDeck /></Route>
+      </main>
+    </ApolloProvider>
   );
 }
 
