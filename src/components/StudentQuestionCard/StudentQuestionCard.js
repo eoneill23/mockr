49bda0ca46ea7128b6074b03c9885b48bf65d095
Djavan Munroe <djavanm24@gmail.com>
2019-10-29 20:29:37 -0600

    Update StudentQuestionCard.js
    Update student question card.

--- src/components/StudentQuestionCard/StudentQuestionCard.js
+++ src/components/StudentQuestionCard/StudentQuestionCard.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-export const StudentQuestionCard = ({ body, notes, id, collapseModal, identifyQuestion, collapsed }) => {  
-
+export const StudentQuestionCard = ({ question, id, collapseModal, identifyQuestion, collapsed }) => {  
+  
+  
   return (
     <section className='student-question'>
-      <p>{body}</p>
-      <p>{notes}</p>
+      <p>{question}</p>
       <button
         onClick={() => { collapseModal(!collapsed); identifyQuestion(id) }}
       >Expand</button>
