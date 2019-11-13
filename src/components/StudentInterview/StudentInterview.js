4e930c38bc05ef94fde9403ff15379c421802652
Eric O'Neill <oneill.eric23@gmail.com>
2019-11-12 19:00:51 -0700

    Begin styling header of student interview modal

--- src/components/StudentInterview/StudentInterview.js
+++ src/components/StudentInterview/StudentInterview.js
@@ -2,29 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export const StudentInterview = ({ interview, id, identifyInterview }) => {
-  const interviewer = interview.users.find(user => user.role !== 0)
-
-  const rubric = ['Skipped 🤡', 'Unsatisfactory 🥺', 'Needs Work 🤨', 'Good 😁', 'Exceptional 🥳'];
-  let eachNote = []
-  eachNote = interview.notes.filter(note => note.score).map(note => {
-    return (
-      <div className='note-summary' key={note.id}>
-        <p>Question: <br />{note.question.body}</p>
-        <p>Score: <br />{rubric[note.score]} </p>
-        <p>Summary: <br />{note.body} </p>
-      </div>
-    )
-  });
-
+  const interviewer = interview.users.find(user => user.role !== 0);
+  
   return (
     <section className='interview-card' onClick={() => identifyInterview(id)}>
       <h3>On {interview.createdAt.split('T')[0]} with {interviewer.firstName} {interviewer.lastName}</h3>
-      {/* <div className='details'>
-        <p className='takeaways' >Score: {rubric[interview.score]} <br/> Takeaways: <br/> {interview.summary}</p><br/>
-       <div className='note-container'>
-        {eachNote}
-       </div> 
-      </div> */}
     </section>
   )
 }
--- src/components/StudentInterviewModal/StudentInterviewModal.js
+++ src/components/StudentInterviewModal/StudentInterviewModal.js
@@ -1,10 +1,50 @@
 import React from 'react';
 
 export const StudentInterviewModal = ({ interview }) => {
-  console.log('AHHHH', interview)
+  console.log(interview)
+  const rubric = ['Skipped 🤡', 'Unsatisfactory 🥺', 'Needs Work 🤨', 'Good 😁', 'Exceptional 🥳'];
+  let eachNote = []
+  eachNote = interview.notes.filter(note => note.score).map(note => {
+    return (
+      <div className='note-summary' key={note.id}>
+        <p className='note-question'>Question: <br />{note.question.body}</p>
+        <p className='note-score'>Score: <br />{rubric[note.score]} </p>
+        <p className='note-notes'>Notes: <br />{note.body} </p>
+      </div>
+    )
+  });
+
   return (
-    <p>{interview.id}</p>
+    <section className='student-interview-modal-content'>
+      <header className='interview-header'><span className='highlight'>Score: </span> {interview.score}<br /><span className='highlight'>Summary: </span>{interview.summary}</header>
+      {eachNote}
+    </section>
   )
 }
 
 export default StudentInterviewModal;
+
+
+// console.log(interview)
+// const interviewer = interview.users.find(user => user.role !== 0)
+
+// const rubric = ['Skipped 🤡', 'Unsatisfactory 🥺', 'Needs Work 🤨', 'Good 😁', 'Exceptional 🥳'];
+// let eachNote = []
+// eachNote = interview.notes.filter(note => note.score).map(note => {
+//   return (
+//     <div className='note-summary' key={note.id}>
+//       <p>Question: <br />{note.question.body}</p>
+//       <p>Score: <br />{rubric[note.score]} </p>
+//       <p>Summary: <br />{note.body} </p>
+//     </div>
+//   )
+// });
+
+// return (
+//   <div className='details'>
+//     <p className='takeaways' >Score: {rubric[interview.score]} <br /> Takeaways: <br /> {interview.summary}</p><br />
+//     <div className='note-container'>
+//       {eachNote}
+//     </div>
+//   </div>
+// )
