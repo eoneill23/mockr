a0e3968ab9d737919951958f1dc77cfbb64bd6a9
Aurie <Myrdden42@gmail.com>
2019-11-14 15:32:20 -0700

    Add Default to Radio Buttons

--- src/components/InterviewNote/InterviewNote.js
+++ src/components/InterviewNote/InterviewNote.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
 export const Note = props => {
+  const getChecked = val => {
+    return val == props.note.score ? 'checked' : '';
+  }
+
   if (props.note) {
     return (
       <div className='interview-note-card shadow'>
@@ -10,10 +14,10 @@ export const Note = props => {
         <textarea name='response' form={'interview-note-response-' + props.id} className='interview-note-response' onChange={event => props.fs.note(props.id, event.target.value)} defaultValue={props.note.body}></textarea>
 
         <form id={'score-form-' + props.id} className='' onChange={event => props.fs.score(props.id, event.target.value)}>
-          <label><input type='radio' name='score' value='1'/>Unsatisfactory<br/></label>
-          <label><input type='radio' name='score' value='2'/>Needs Work<br/></label>
-          <label><input type='radio' name='score' value='3'/>Good<br/></label>
-          <label><input type='radio' name='score' value='4'/>Exceptional<br/></label>
+          <label><input type='radio' name='score' value='1' checked={getChecked(1)}/>Unsatisfactory<br/></label>
+          <label><input type='radio' name='score' value='2' checked={getChecked(2)}/>Needs Work<br/></label>
+          <label><input type='radio' name='score' value='3' checked={getChecked(3)}/>Good<br/></label>
+          <label><input type='radio' name='score' value='4' checked={getChecked(4)}/>Exceptional<br/></label>
         </form>
       </div>
     );
