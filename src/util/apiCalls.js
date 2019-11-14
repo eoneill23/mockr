601ec8712ebe1af9c4e65128a9c1c182d939f22f
Aurie <Myrdden42@gmail.com>
2019-11-14 16:35:28 -0700

    Add Role Request Query to ApiCalls

--- src/util/apiCalls.js
+++ src/util/apiCalls.js
@@ -56,6 +56,7 @@ export const USER = gql`
       lastName
       email
       role
+      roleRequest
       interviews {
         id
         score
@@ -78,125 +79,135 @@ export const USER = gql`
       }
     }
   }
-  `;
+`;
 
-  export const LOGIN = gql`
-  query Login($email: String!, $password: String!){
-    login(email: $email, password: $password) {
+export const USER_ROLE_REQUEST = gql`
+  mutation UserRoleRequest($id: Int!, $role: Int!){
+    updateUser(id: $id, roleRequest: $role) {
       id
-      firstName
-      lastName
-      email
-      role
-      interviews {
+      roleRequest
+    }
+  }
+`;
+
+
+export const LOGIN = gql`
+query Login($email: String!, $password: String!){
+  login(email: $email, password: $password) {
+    id
+    firstName
+    lastName
+    email
+    role
+    interviews {
+      id
+      score
+      summary
+      createdAt
+      users {
+        firstName
+        lastName
+        role
+      }
+      notes {
         id
+        body
         score
-        summary
-        createdAt
-        users {
-          firstName
-          lastName
-          role
-        }
-        notes {
+        question {
           id
           body
-          score
-          question {
-            id
-            body
-          }
         }
       }
     }
   }
-  `;
+}
+`;
 
 export const ALL_STUDENTS = gql`
-  query {
-    users(role: 0) {
-      id
-      firstName
-      lastName
-      email
-      program
-      cohort
-    }
+query {
+  users(role: 0) {
+    id
+    firstName
+    lastName
+    email
+    program
+    cohort
   }
+}
 `
 
 export const ALL_QUESTIONS = gql`
-  query {
-    questions
-    {
+query {
+  questions
+  {
+    id
+    body
+    active
+  }
+}
+`;
+
+export const ADD_QUESTION = gql`
+mutation AddQuestion($body: String!) {
+  addQuestion(body: $body) {
+    id
+    body
+  }
+}
+`;
+
+export const RANDOM_QUESTIONS = gql`
+query {
+  randomQuestions {
+    id
+    body
+  }
+}
+`;
+
+export const CREATE_INTERVIEW = gql`
+mutation CreateInterview($studentId: Int!, $interviewerId: Int!) {
+  addInterview(studentId: $studentId, interviewerId: $interviewerId) {
+    id
+    users {
       id
-      body
+      firstName
+      lastName
+    }
+  }
+}
+`;
+
+export const ADD_NOTE = gql`
+mutation AddNote($score: Int!, $body: String!, $questionId: Int!, $interviewId: Int!, $studentId: Int!, $interviewerId: Int!) {
+  addNote(score: $score, body: $body, questionId: $questionId, interviewId: $interviewId, studentId: $studentId, interviewerId: $interviewerId) {
+    id
+  }
+}
+`;
+
+export const FINALISE_INTERVIEW = gql`
+mutation FinaliseInterview($id: Int!, $score: Int!, $body: String!) {
+  finalizeInterview(id: $id, score: $score, summary: $body) {
+    id
+  }
+}
+`;
+
+export const ACTIVATE_QUESTION = gql`
+  mutation ActivateQuestion($id: Int!, $active: Boolean!) {
+    activateQuestion(id: $id, active: $active) {
+      id
       active
     }
   }
 `;
 
-export const ADD_QUESTION = gql`
-  mutation AddQuestion($body: String!) {
-    addQuestion(body: $body) {
-      id
-      body
-    }
-  }
-`;
-
-export const RANDOM_QUESTIONS = gql`
-  query {
-    randomQuestions {
-      id
-      body
-    }
-  }
-`;
-
-export const CREATE_INTERVIEW = gql`
-  mutation CreateInterview($studentId: Int!, $interviewerId: Int!) {
-    addInterview(studentId: $studentId, interviewerId: $interviewerId) {
-      id
-      users {
-        id
-        firstName
-        lastName
-      }
-    }
-  }
-`;
-
-export const ADD_NOTE = gql`
-  mutation AddNote($score: Int!, $body: String!, $questionId: Int!, $interviewId: Int!, $studentId: Int!, $interviewerId: Int!) {
-    addNote(score: $score, body: $body, questionId: $questionId, interviewId: $interviewId, studentId: $studentId, interviewerId: $interviewerId) {
-      id
-    }
-  }
-`;
-
-export const FINALISE_INTERVIEW = gql`
-  mutation FinaliseInterview($id: Int!, $score: Int!, $body: String!) {
-    finalizeInterview(id: $id, score: $score, summary: $body) {
-      id
-    }
-  }
-`;
-
-export const ACTIVATE_QUESTION = gql`
-    mutation ActivateQuestion($id: Int!, $active: Boolean!) {
-      activateQuestion(id: $id, active: $active) {
-        id
-        active
-      }
-    }
-`;
-
 export const DEACTIVATE_QUESTION = gql`
-    mutation ActivateQuestion($id: Int!, $active: Boolean!) {
-      activateQuestion(id: $id, active: $active) {
-        id
-        active
-      }
+  mutation ActivateQuestion($id: Int!, $active: Boolean!) {
+    activateQuestion(id: $id, active: $active) {
+      id
+      active
     }
+  }
 `;
